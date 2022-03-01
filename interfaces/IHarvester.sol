8d9fde3861d9b5e6a2abee903b22d43d52318399
pradeep-selva <pradeepselvaraju@gmail.com>
2022-03-02 01:29:36 +0530

    bg: harvester added

---
+++ interfaces/IHarvester.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.0;
+
+interface IHarvester {
+    function strategist() external view returns (address);
+
+    function governance() external view returns (address);
+
+    function slippage() external view returns (uint256);
+
+    function swapTokens(uint256 idx) external returns (address);
+
+    function numTokens() external returns (uint256);
+
+    function positionHandlers(address handler) external view returns (bool);
+
+    function addPositionHandler(address _positionHandler) external;
+
+    function removePositionHandler(address _positionHandler) external;
+
+    function setWantToken(address _addr) external;
+
+    function setStrategist(address _strategist) external;
+
+    function setGovernance(address _governance) external;
+
+    function setSlippage(uint256 _slippage) external;
+
+    // Add token to be swapped via 1inch
+    function addSwapToken(address _addr) external;
+
+    // Remove token to be swapped via 1inch
+    function removeSwapToken(address _addr) external;
+
+    // Swap tokens to wantToken
+    function harvest() external;
+
+    //1inch swap config add any required params
+    function swap(address sourceToken) external;
+
+    function sweep(address _token) external;
+
+    function migrate(address _newHarvester) external;
+}
