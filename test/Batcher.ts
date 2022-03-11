0d199e6a38b28f010b1f1349767574e86525dc44
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-03-11 22:05:54 +0530

    batcher deposit verified

--- test/Batcher.ts
+++ test/Batcher.ts
@@ -1,8 +1,10 @@
 import { expect } from "chai";
 import hre from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import {Batcher} from "../src/types";
-import {setup} from "./utils";
+import {Batcher, ERC20} from "../src/types";
+import {setup, getSignature} from "./utils";
+import { BigNumber } from "ethers";
+import { sign } from "crypto";
 
 
 describe("Batcher", function () {
@@ -11,20 +13,35 @@ describe("Batcher", function () {
     let governanceAddress: string;
     let signer: SignerWithAddress;
     let invalidSigner: SignerWithAddress;
-
+    let routerAddres: string;
     before(async () => {
         [keeperAddress, governanceAddress, signer, invalidSigner] = await setup();
+        routerAddres = "0x95Ba4cF87D6723ad9C0Db21737D862bE80e93911";
      });
 
     it("Check address assingment Batcher", async function () {
         const Batcher = await hre.ethers.getContractFactory("Batcher", signer);
-        batcher = (await Batcher.deploy(governanceAddress, governanceAddress)) as Batcher;
+        batcher = (await Batcher.deploy(invalidSigner.address, governanceAddress)) as Batcher;
         await batcher.deployed();
         expect(await batcher.governance()).to.equal(governanceAddress);
         expect(await batcher.owner()).to.equal(signer.address);
-        expect(await batcher.verificationAuthority()).to.equal(governanceAddress);
+        expect(await batcher.verificationAuthority()).to.equal(invalidSigner.address);
     
     });
 
+    it("Deposit verification", async function () {
+       let signature =  await getSignature(signer.address, invalidSigner, batcher.address);
+        console.log("signature:", signature);
+        let amount = BigNumber.from(100e6);
+        const USDC = (await hre.ethers.getContractAt(
+          "ERC20",
+          "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        )) as ERC20;
+        await USDC.connect(signer).approve(batcher.address, amount);
+        await batcher.setRouter(routerAddres, USDC.address, BigNumber.from(1000e6));
+
+        await batcher.depositFunds(amount, routerAddres ,signature);
+        expect(await batcher.depositLedger(routerAddres, signer.address)).to.equal(amount);
+    });
 
 });
