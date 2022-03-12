b5fb20152ec7408d904b4badc2ef0d8d7941efe1
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-03-12 12:35:30 +0530

    completed testing of deposit/withdraw

--- test/Batcher.ts
+++ test/Batcher.ts
@@ -35,6 +35,7 @@ describe("Batcher", function () {
     // depositFunds -  increament in depositLedger mapping, batcher balance increament,
     //              -  decrease of USDC funds of user, message for user only verified
     //              - shouldn't breach maxDepositLimit of rotuer.  
+    // batchDeposit - onlyOwner should call this function
 
     it("Deposit verification", async function () {
        let signature =  await getSignature(signer.address, invalidSigner, batcher.address);
@@ -49,27 +50,40 @@ describe("Batcher", function () {
 
         await batcher.depositFunds(amount, router.address ,signature);
         expect(await batcher.depositLedger(router.address, signer.address)).to.equal(amount);
-
+        await expect(
+            batcher.connect(invalidSigner).batchDeposit(router.address, [signer.address])
+        ).to.be.revertedWith("Ownable: caller is not the owner");
         await batcher.batchDeposit(router.address, [signer.address]);
         expect(await batcher.depositLedger(router.address, signer.address)).to.equal(BigNumber.from(0));
-
+        expect(await router.balanceOf(signer.address)).to.equal(amount);
     });
 
     // Operation - Expected Behaviour
     // withdrawFunds -  increament in withdrawLedger mapping, batcher balance increament in router tokens.
     //              -  increase of USDC funds of user.
+    // batchWithdraw - onlyOwner should call this function, with decrease in router tokens.
+
     it("Withdraw verification", async function () {
-
-    });
-
-    // Operation - Expected Behaviour
-    // batchDeposit - onlyOwner should call this function, user usdc balance should decrease with increase in router tokens.
-    it("Batch deposit verification", async function () {
-    });
-
-    // Operation - Expected Behaviour
-    // batchWithdraw - onlyOwner should call this function, user usdc balance should increase with decrease in router tokens.
-    it("Batch withdraw verification", async function () {
+        let amount = BigNumber.from(100e6);
+        const USDC = (await hre.ethers.getContractAt(
+          "ERC20",
+          "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        )) as ERC20;
+        await USDC.connect(signer).approve(router.address, amount);
+        await router.deposit(amount, signer.address);
+        await router.connect(signer).approve(batcher.address, amount);
+        await expect(
+            batcher.connect(invalidSigner).batchWithdraw(router.address, [signer.address])
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await batcher.withdrawFunds(amount, router.address);
+        expect(await batcher.withdrawLedger(router.address, signer.address)).to.equal(amount);
+        
+        expect(await router.balanceOf(batcher.address)).to.equal(amount);
+        let balanceBefore = await USDC.balanceOf(signer.address);
+        await batcher.batchWithdraw(router.address, [signer.address]);
+        let balanceAfter = await USDC.balanceOf(signer.address);
+        expect(await batcher.withdrawLedger(router.address, signer.address)).to.equal(BigNumber.from(0));
+        expect(balanceAfter.sub(balanceBefore)).to.equal(amount);
     });
 
 
