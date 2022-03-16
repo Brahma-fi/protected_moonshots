33d3fd8f70dbe9ed1ed6358a93036ef1b5e5a757
OGZ <adi.gupta13@gmail.com>
2022-03-16 23:26:14 +0530

    Editing existing constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -4,8 +4,10 @@ export const ust3Pool = "0xCEAF7747579696A2F0bb206a14210e3c9e6fB269";
 export const curve3PoolZap = "0xA79828DF1850E8a3A3064576f380D90aECDD3359";
 export const baseRewardPool = "0x7e2b9B5244bcFa5108A76D5E7b507CFD5581AD4A";
 
-export const keeper = "";
-export const governance = "";
+export const keeper = "0x55FE002aefF02F77364de339a1292923A15844B8";
+export const governance = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
+
+export const optimismL1CrossDomainMessenger = "0x25ace71c97B33Cc4729CF772ae268934F7ab5fA1";
 
 export const optimismRPCPort = 8550;
 export const optimismRPCBlock = 4462648;
--- test/PerpTE.ts
+++ test/PerpTE.ts
@@ -2,17 +2,17 @@ import { expect } from "chai";
 import hre from "hardhat";
 import dotenv from "dotenv";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import {Batcher, Hauler, ICurveDepositZapper, ICurvePool} from "../src/types";
+import {Batcher, Hauler, ICurveDepositZapper, ICurvePool, PerpPositionHandler, PerpTradeExecutor, PerpPositionHandlerL2} from "../src/types";
 import {setup, getSignature, getHaulerContract, getUSDCContract} from "./utils";
 import { BigNumber, BigNumberish } from "ethers";
-import { optimismRPCBlock, optimismRPCPort } from "../scripts/constants";
+import { optimismRPCBlock, optimismRPCPort, wantTokenL1, wantTokenL2, optimismL1CrossDomainMessenger, } from "../scripts/constants";
 import { ChildProcess, exec } from 'child_process';
 import { Provider } from "@ethersproject/abstract-provider";
 import { JsonRpcProvider } from "@ethersproject/providers";
 
 // Language: typescript
 // Path: test/PerpTE.ts
-// Main access modifiers owner, governor.
+// Main access modifiers governance, keeper.
 
 let optimismForkRPC:ChildProcess;
 
@@ -22,11 +22,15 @@ describe.only("PerpTE", function () {
     let keeperAddress: string;
     let governanceAddress: string;
     let signer: SignerWithAddress;
+    let signerL2: SignerWithAddress;
     let invalidSigner: SignerWithAddress;
     let hauler: Hauler;
 
     let l1Provider: JsonRpcProvider;
     let l2Provider: JsonRpcProvider;
+
+    let perpTE: PerpTradeExecutor;
+    let perpL2Handler: PerpPositionHandlerL2;
 
     
     
@@ -39,20 +43,53 @@ describe.only("PerpTE", function () {
                 console.log('exec error: ' + error);
             }
         });
-        await new Promise(r => setTimeout(r, 10000));
 
-        l1Provider = hre.ethers.provider;
-        l2Provider = new hre.ethers.providers.JsonRpcProvider('http://localhost:8550');
+        try {
+            await new Promise(r => setTimeout(r, 10000));
 
-        [keeperAddress, governanceAddress, signer, invalidSigner] = await setup();
+            l1Provider = hre.ethers.provider;
+            l2Provider = new hre.ethers.providers.JsonRpcProvider('http://127.0.0.1:8550');
 
+            [keeperAddress, governanceAddress, signer, invalidSigner] = await setup();
+            hauler = await getHaulerContract();
+
+            const signerL2 = new hre.ethers.Wallet(process.env.PRIVATE_KEY, l2Provider);
+            console.log(signerL2)
+
+            const PerpL2Handler = await hre.ethers.getContractFactory("PerpPositionHandlerL2", signerL2);
+            perpL2Handler = await PerpL2Handler.deploy() as PerpPositionHandlerL2;
+            await perpL2Handler.deployed();
+
+            // console.log(PerpL2Handler.getDeployTransaction().chainId);
+
+            const PerpTE = await hre.ethers.getContractFactory("PerpTradeExecutor", signer);
+            perpTE = await PerpTE.deploy(
+                hauler.address,
+                wantTokenL2,
+                perpL2Handler.address,
+                optimismL1CrossDomainMessenger
+            ) as PerpTradeExecutor;
+
+            await perpTE.deployed();
+
+            console.log(PerpTE.getDeployTransaction(hauler.address,
+                wantTokenL2,
+                perpL2Handler.address,
+                optimismL1CrossDomainMessenger).chainId);
+
+
+        } catch (err) {
+            console.log(err);
+            optimismForkRPC.kill();
+        }
      });
 
-    it("Check address assingment Batcher", async function () {
+    it("L1 and L2 contracts initialized correctly", async function () {
 
 
 
-        console.log('nysigner', signer.address);
+        console.log('l1signer', signer.address);
+        console.log('l2signer', signerL2.address);
         
         const l1bal = await l1Provider.getBalance("0xAE75B29ADe678372D77A8B41225654138a7E6ff1");
         console.log('l1 bal', l1bal.toString());
@@ -61,6 +98,8 @@ describe.only("PerpTE", function () {
         const l2bal = await l2Provider.getBalance("0xAE75B29ADe678372D77A8B41225654138a7E6ff1");
         console.log('l2 bal', l2bal.toString());
 
+
+        optimismForkRPC.kill(0);
     
     });
     after( async() => {
