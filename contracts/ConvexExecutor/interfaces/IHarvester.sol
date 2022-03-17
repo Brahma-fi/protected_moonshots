d5aaea5707eb095a8f18e31141f0831133358560
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-03-17 11:49:17 +0530

    added swap using crv and uniswap

--- contracts/ConvexExecutor/interfaces/IHarvester.sol
+++ contracts/ConvexExecutor/interfaces/IHarvester.sol
@@ -8,10 +8,6 @@ interface IHarvester {
 
   function slippage() external view returns (uint256);
 
-  function swapTokens(uint256 idx) external returns (address);
-
-  function numTokens() external returns (uint256);
-
   function setWantToken(address _addr) external;
 
   function setKeeper(address _keeper) external;
@@ -20,19 +16,11 @@ interface IHarvester {
 
   function setSlippage(uint256 _slippage) external;
 
-  // Add token to be swapped via 1inch
-  function addSwapToken(address _addr) external;
-
-  // Remove token to be swapped via 1inch
-  function removeSwapToken(address _addr) external;
-
   // Swap tokens to wantToken
   function harvest() external;
 
-  //1inch swap config add any required params
-  function swap(address sourceToken) external;
-
   function sweep(address _token) external;
 
-  function migrate(address _newHarvester) external;
+  function approve() external;
+
 }
