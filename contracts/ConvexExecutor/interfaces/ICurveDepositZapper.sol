1fc8fabf7e7835b09ee19f9235fc19da6378ed29
pradeep-selva <pradeepselvaraju@gmail.com>
2022-03-17 17:39:00 +0530

    ft: lp-> usdc & usdc-> lp estimation fix

--- contracts/ConvexExecutor/interfaces/ICurveDepositZapper.sol
+++ contracts/ConvexExecutor/interfaces/ICurveDepositZapper.sol
@@ -6,13 +6,13 @@ interface ICurveDepositZapper {
     address _pool,
     uint256 _token_amount,
     int128 i
-  ) external returns (uint256);
+  ) external view returns (uint256);
 
   function calc_token_amount(
     address _pool,
     uint256[4] memory _amounts,
     bool _is_deposit
-  ) external returns (uint256);
+  ) external view returns (uint256);
 
   function remove_liquidity_one_coin(
     address _pool,
