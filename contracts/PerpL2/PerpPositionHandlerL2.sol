1bcf30b7efdb1a045c42462ca35f0593f40aa68b
OGZ <adi.gupta13@gmail.com>
2022-03-23 00:58:16 +0530

    comitting perp handler changes

--- contracts/PerpL2/PerpPositionHandlerL2.sol
+++ contracts/PerpL2/PerpPositionHandlerL2.sol
@@ -6,17 +6,23 @@ import "./OptimismL2Wrapper.sol";
 import "./MovrV1Controller.sol";
 import "./interfaces/IPositionHandler.sol";
 
-import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import {SafeMathUpgradeable} from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 import "./interfaces/IERC20.sol";
 
 /// @title PerpPositionHandlerL2
 /// @author 0xAd1
 /// @notice Acts as positon handler and token bridger on L2 Optimism
-contract PerpPositionHandlerL2 is IPositionHandler, PerpV2Controller, MovrV1Controller, OptimismL2Wrapper{
+contract PerpPositionHandlerL2 is
+    IPositionHandler,
+    PerpV2Controller,
+    MovrV1Controller,
+    OptimismL2Wrapper
+{
     using SafeMathUpgradeable for uint256;
 
     struct PerpPosition {
-        uint256 entryMarkTwap;
+        uint256 entryMarkPrice;
+        uint256 entryIndexPrice;
         uint256 entryAmount;
         bool isShort;
         bool isActive;
@@ -32,16 +38,17 @@ contract PerpPositionHandlerL2 is IPositionHandler, PerpV2Controller, MovrV1Cont
 
     /// @notice SPHL1 getter
     /// @return address of SPHL1 contract
-    address public SPHL1Address;
+    address public positionHandlerL1;
+    address public movrRegistry;
 
     address public keeper;
 
     PerpPosition public perpPosition;
 
-    function init(
+    constructor(
         address _wantTokenL1,
         address _wantTokenL2,
-        address _SPHL1Address,
+        address _positionHandlerL1,
         address _perpVault,
         address _clearingHouse,
         address _clearingHouseConfig,
@@ -50,11 +57,44 @@ contract PerpPositionHandlerL2 is IPositionHandler, PerpV2Controller, MovrV1Cont
         address _exchange,
         address _baseTokenvCRV,
         address _quoteTokenvUSDC,
-        address _keeper
-    ) public onlyAuthorized {
+        address _keeper,
+        address _movrRegistry
+    ) {
+        init(
+            _wantTokenL1,
+            _wantTokenL2,
+            _positionHandlerL1,
+            _perpVault,
+            _clearingHouse,
+            _clearingHouseConfig,
+            _accountBalance,
+            _orderBook,
+            _exchange,
+            _baseTokenvCRV,
+            _quoteTokenvUSDC,
+            _keeper,
+            _movrRegistry
+        );
+    }
+
+    function init(
+        address _wantTokenL1,
+        address _wantTokenL2,
+        address _positionHandlerL1,
+        address _perpVault,
+        address _clearingHouse,
+        address _clearingHouseConfig,
+        address _accountBalance,
+        address _orderBook,
+        address _exchange,
+        address _baseTokenvCRV,
+        address _quoteTokenvUSDC,
+        address _keeper,
+        address _movrRegistry
+    ) internal {
         wantTokenL1 = _wantTokenL1;
         wantTokenL2 = _wantTokenL2;
-        SPHL1Address = _SPHL1Address;
+        positionHandlerL1 = _positionHandlerL1;
         perpVault = IVault(_perpVault);
         clearingHouse = IClearingHouse(_clearingHouse);
         clearingHouseConfig = IClearingHouseConfig(_clearingHouseConfig);
@@ -64,57 +104,82 @@ contract PerpPositionHandlerL2 is IPositionHandler, PerpV2Controller, MovrV1Cont
         baseTokenvCRV = IERC20(_baseTokenvCRV);
         quoteTokenvUSDC = IERC20(_quoteTokenvUSDC);
         keeper = _keeper;
+        movrRegistry = _movrRegistry;
     }
 
-
-
-
     /// @inheritdoc IPositionHandler
-    // opens short position by default and accepts 
-    function openPosition(bool isShort, uint256 amountIn, uint24 slippage) public override onlyAuthorized{
-        require (perpPosition.isActive == false, "Position already open");
+    // opens short position by default and accepts
+    function openPosition(
+        bool isShort,
+        uint256 amountIn,
+        uint24 slippage
+    ) public override onlyAuthorized {
+        require(perpPosition.isActive == false, "Position already open");
         uint256 wantTokenBalance = IERC20(wantTokenL2).balanceOf(address(this));
         _depositToPerp(wantTokenBalance);
-        perpPosition = PerpPosition({entryMarkTwap: formatSqrtPriceX96(getTwapPrice()), entryAmount: amountIn, isShort: isShort, isActive: true});
+        perpPosition = PerpPosition({
+            entryMarkPrice: formatSqrtPriceX96(getMarkTwapPrice()),
+            entryIndexPrice: getIndexTwapPrice(),
+            entryAmount: amountIn,
+            isShort: isShort,
+            isActive: true
+        });
         _openPositionByAmount(isShort, amountIn, slippage);
     }
 
     /// @inheritdoc IPositionHandler
-    function closePosition(uint24 slippage)
-        public
-        override
-        onlyAuthorized
-    {
+    function closePosition(uint24 slippage) public override onlyAuthorized {
         require(perpPosition.isActive, "No active position");
         _closePosition(slippage);
         perpPosition.isActive = false;
         _withdrawFromPerp(getFreeCollateral());
-
-
     }
 
     /// @inheritdoc IPositionHandler
-    function withdraw(uint256 amountOut, address allowanceTarget, address movrRegistry, bytes calldata movrData) public override onlyAuthorized{
-        require(IERC20(wantTokenL2).balanceOf(address(this)) >= amountOut, "Insufficient balance");
-        sendWantTokens(wantTokenL2, allowanceTarget, movrRegistry, amountOut, movrData);
-
+    function withdraw(
+        uint256 amountOut,
+        address allowanceTarget,
+        address _movrRegistry,
+        bytes calldata movrData
+    ) public override onlyAuthorized {
+        require(
+            IERC20(wantTokenL2).balanceOf(address(this)) >= amountOut,
+            "Insufficient balance"
+        );
+        require(movrRegistry == _movrRegistry, "Invalid movr registry");
+        sendTokens(
+            wantTokenL2,
+            allowanceTarget,
+            movrRegistry,
+            positionHandlerL1, 
+            amountOut,
+            1, /// TODO: should hardcode destination chain 1 or accept argument??
+            movrData
+        );
     }
 
     /// @inheritdoc IPositionHandler
-    function sweep(address _token) public override onlyAuthorized{
+    function sweep(address _token) public override onlyAuthorized {
         IERC20(_token).transfer(
             msg.sender,
             IERC20(_token).balanceOf(address(this))
         );
     }
 
-    function setReferralCode(bytes32 _referralCode) onlyAuthorized public {
+    function setReferralCode(bytes32 _referralCode) public onlyAuthorized {
         referralCode = _referralCode;
     }
 
-    modifier onlyAuthorized {
-        require(((msg.sender == L2CrossDomainMessenger && messageSender() == SPHL1Address) || msg.sender == keeper), "Only owner can call this function");
+    function setMovrRegistry(address _movrRegistry) public onlyAuthorized {
+        movrRegistry = _movrRegistry;
+    }
+
+    modifier onlyAuthorized() {
+        require(
+            ((msg.sender == L2CrossDomainMessenger &&
+                messageSender() == positionHandlerL1) || msg.sender == keeper),
+            "Only owner can call this function"
+        );
         _;
     }
- 
 }
--- contracts/PerpL2/PerpV2Controller.sol
+++ contracts/PerpL2/PerpV2Controller.sol
@@ -10,6 +10,8 @@ import "@perp/curie-contract/contracts/interface/IVirtualToken.sol";
 import "@perp/curie-contract/contracts/interface/IOrderBook.sol";
 import "@perp/curie-contract/contracts/interface/IExchange.sol";
 import "@perp/curie-contract/contracts/interface/IClearingHouseConfig.sol";
+import "@perp/curie-contract/contracts/interface/IIndexPrice.sol";
+
 
 import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
 import { SignedSafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SignedSafeMathUpgradeable.sol";
@@ -22,30 +24,21 @@ contract PerpV2Controller {
     using SignedSafeMathUpgradeable for int256;
     using SafeMathUpgradeable for uint256;
 
-    event Damn(uint amountIn, uint amountOut, uint price);
-
-    // // USDC token 
-    // TODO: init   
-    // IERC20 public USDCToken = IERC20(0x3e22e37Cb472c872B5dE121134cFD1B57Ef06560);
-
-    // Perp Internal Tokens
-    // TODO: init
-    IERC20 public baseTokenvCRV = IERC20(0x7cE531e940B24EE760e5dDBF8A2E0E359fA04400);
-    IERC20 public quoteTokenvUSDC = IERC20(0xd52d4175F937B965dE49E6C24E081eEe6DaE5645);
+    IERC20 public baseTokenvCRV;
+    IERC20 public quoteTokenvUSDC;
 
     bytes32 public referralCode; 
 
     // Perp contracts - Kovan Optimism
     // TODO: init
-    IVault public perpVault = IVault(0x87113069Cd05E819B1F009BEEC70dd41594A9D12);
-    IClearingHouse public clearingHouse = IClearingHouse(0x21BFC3ee9f1B3156b446654C571433ff05147153);
-    IClearingHouseConfig public clearingHouseConfig = IClearingHouseConfig(0x3c99fFe490C6EBd18A2CF9E7d59669a0113802Dd);
-    IAccountBalance public accountBalance = IAccountBalance(0xB35669c4F64faBBF71dc2e4307485Ad74d46f4E8);
-    IOrderBook public orderBook = IOrderBook(0x6155d88B3939493A1bba90959dEEea5Bf1dDa72c);
-    IExchange public exchange = IExchange(0x870fdaC8DD38c727C1daa67F088ed0926Fb5fd4d);
+    IVault public perpVault;
+    IClearingHouse public clearingHouse;
+    IClearingHouseConfig public clearingHouseConfig;
+    IAccountBalance public accountBalance;
+    IOrderBook public orderBook;
+    IExchange public exchange;
 
 
-    uint256 public markPriceOnLastPosition = 0;
     /// VIEW / CALL
 
     /// @notice Get free collateral available on Perp V2
@@ -54,11 +47,18 @@ contract PerpV2Controller {
         return perpVault.getFreeCollateral(address(this));
     }
 
-    /// @notice Gets twap price of Perp's uniswap pool (quoteToken/baseToken)
+    /// @notice Gets mark twap price of Perp's uniswap pool (quoteToken/baseToken)
     /// @return uint160 gets SqrtX96 price
-    function getTwapPrice() public view returns (uint160) {
+    function getMarkTwapPrice() public view returns (uint160) {
         uint32 twapInterval = clearingHouseConfig.getTwapInterval();
         uint160 twapPrice = exchange.getSqrtMarkTwapX96(address(baseTokenvCRV), twapInterval);
+        return twapPrice;
+    }
+
+
+    function getIndexTwapPrice() public view returns (uint256) {
+        uint32 twapInterval = clearingHouseConfig.getTwapInterval();
+        uint256 twapPrice = IIndexPrice(address(baseTokenvCRV)).getIndexPrice(twapInterval);
         return twapPrice;
     }
 
@@ -111,7 +111,7 @@ contract PerpV2Controller {
     /// @param slippage slippage while opening position
     function _openPositionByAmount(bool short, uint256 amountIn, uint24 slippage) internal returns (int256 positionSize){
 
-        uint256 price = formatSqrtPriceX96(getTwapPrice());
+        uint256 price = formatSqrtPriceX96(getMarkTwapPrice());
         
         // (amountIn/price) * (1-slippage)
         // 1- 0.005 = 0.995
@@ -138,7 +138,7 @@ contract PerpV2Controller {
     /// @param slippage slippage while opening position
     function _closePosition(uint slippage) internal {
         
-        uint256 price = formatSqrtPriceX96(getTwapPrice());
+        uint256 price = formatSqrtPriceX96(getMarkTwapPrice());
         uint256 amountOut = (getTotalPerpPositionSize() < 0) ? uint256(-1*getTotalPerpPositionSize()) : uint256(getTotalPerpPositionSize());
         
         amountOut = (getTotalPerpPositionSize() < 0) ? amountOut.mul(10**4+slippage).div(10**4) : amountOut.mul(10**4-slippage).div(10**4);
