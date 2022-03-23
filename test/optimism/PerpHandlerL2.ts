0333ea157bd33d5f58206daa99554398727afa19
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-03-23 17:30:02 +0530

    formating files

--- test/optimism/PerpHandlerL2.ts
+++ test/optimism/PerpHandlerL2.ts
@@ -1,196 +1,268 @@
 import { expect } from "chai";
 import hre from "hardhat";
-import dotenv from "dotenv";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { Batcher, Hauler, IERC20, ICurveDepositZapper, ICurvePool, PerpPositionHandler, PerpTradeExecutor, PerpPositionHandlerL2, IClearingHouseConfig, IIndexPrice, IAccountBalance } from "../../src/types";
-import { setup, getSignature, getHaulerContract, getUSDCContract } from "../utils";
-import { BigNumber, BigNumberish } from "ethers";
-import { optimismRPCBlock, optimismRPCPort, wantTokenL1, wantTokenL2, optimismL1CrossDomainMessenger, perpVault, clearingHouse, clearingHouseConfig, accountBalance, orderBook, exchange, baseTokenvCRV, quoteTokenvUSDC, movrRegistry} from "../../scripts/constants";
-import { ChildProcess, exec } from 'child_process';
-import { Provider } from "@ethersproject/abstract-provider";
-import { JsonRpcProvider } from "@ethersproject/providers";
+import {
+  Hauler,
+  IERC20,
+  PerpPositionHandlerL2,
+  IClearingHouseConfig,
+  IIndexPrice,
+  IAccountBalance,
+} from "../../src/types";
+import { BigNumber } from "ethers";
+import {
+  optimismRPCBlock,
+  optimismRPCPort,
+  wantTokenL1,
+  wantTokenL2,
+  optimismL1CrossDomainMessenger,
+  perpVault,
+  clearingHouse,
+  clearingHouseConfig,
+  accountBalance,
+  orderBook,
+  exchange,
+  baseToken,
+  quoteTokenvUSDC,
+  movrRegistry,
+} from "../../scripts/constants";
 
-import {moverCall} from "../api";
-
-
-
+import { moverCall } from "../api";
 
 describe.only("PerpHandlerL2 [OPTIMISM]", function () {
-    let keeperAddress: string;
-    let governanceAddress: string;
-    let signer: SignerWithAddress;
-    let signerL2: SignerWithAddress;
-    let invalidSigner: SignerWithAddress;
-    let hauler: Hauler;
+  let keeperAddress: string;
+  let governanceAddress: string;
+  let signer: SignerWithAddress;
+  let signerL2: SignerWithAddress;
+  let invalidSigner: SignerWithAddress;
+  let hauler: Hauler;
 
-    let perpL2Handler: PerpPositionHandlerL2;
-    let USDC: IERC20;
+  let perpL2Handler: PerpPositionHandlerL2;
+  let USDC: IERC20;
 
-    let clearingHouseConfigContract: IClearingHouseConfig;
-    let accountBalanceContract: IAccountBalance;
+  let clearingHouseConfigContract: IClearingHouseConfig;
+  let accountBalanceContract: IAccountBalance;
 
-
-
-    before(async () => {
-
-        await hre.network.provider.request({
-            method: "hardhat_impersonateAccount",
-            params: ["0x45af3Bd5A2c60B7410f33C313c247c439b633446"]
-        });
-
-        signer = await hre.ethers.getSigner("0x45af3Bd5A2c60B7410f33C313c247c439b633446");
-
-        await hre.network.provider.request({
-            method: "hardhat_setBalance",
-            params: ["0x45af3Bd5A2c60B7410f33C313c247c439b633446", "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"]
-        });
-
-        await hre.network.provider.request({
-            method: "hardhat_impersonateAccount",
-            params: ["0xAE75B29ADe678372D77A8B41225654138a7E6ff1"]
-        });
-
-        invalidSigner = await hre.ethers.getSigner("0xAE75B29ADe678372D77A8B41225654138a7E6ff1");
-
-        const PerpHandlerL2 = await hre.ethers.getContractFactory("PerpPositionHandlerL2", invalidSigner);
-        perpL2Handler = await PerpHandlerL2.deploy(
-            wantTokenL1,
-            wantTokenL2,
-            invalidSigner.address,
-            perpVault,
-            clearingHouse,
-            clearingHouseConfig,
-            accountBalance,
-            orderBook,
-            exchange,
-            baseTokenvCRV,
-            quoteTokenvUSDC,
-            signer.address,
-            movrRegistry,
-        ) as PerpPositionHandlerL2;
-
-        await perpL2Handler.deployed();
-
-        USDC = await hre.ethers.getContractAt("@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20", wantTokenL2) as IERC20;
-        await USDC.connect(invalidSigner).transfer(perpL2Handler.address, (await USDC.balanceOf(invalidSigner.address)));
-
-        const baseToken = await hre.ethers.getContractAt("IIndexPrice", baseTokenvCRV) as IIndexPrice;
-        clearingHouseConfigContract = await hre.ethers.getContractAt("IClearingHouseConfig", clearingHouseConfig) as IClearingHouseConfig;
-        const twapInterval = await clearingHouseConfigContract.getTwapInterval();
-        accountBalanceContract = await hre.ethers.getContractAt("IAccountBalance", accountBalance) as IAccountBalance;
-
-        
-
+  before(async () => {
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0x45af3Bd5A2c60B7410f33C313c247c439b633446"],
     });
 
-    it("Contract initialized correctly", async function () {
+    signer = await hre.ethers.getSigner(
+      "0x45af3Bd5A2c60B7410f33C313c247c439b633446"
+    );
 
-        expect(await perpL2Handler.keeper()).equals(signer.address);
-
-        const usdcBal = await USDC.balanceOf(perpL2Handler.address);
-
-        expect(usdcBal.gt(0));
+    await hre.network.provider.request({
+      method: "hardhat_setBalance",
+      params: [
+        "0x45af3Bd5A2c60B7410f33C313c247c439b633446",
+        "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+      ],
     });
 
-    it("Can open short", async function () {
-
-        let usdcBal = await USDC.balanceOf(perpL2Handler.address);
-        usdcBal = usdcBal.mul(1e12);
-
-        equal((await perpL2Handler.perpPosition()).isActive, false);
-
-        equal(await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV), 0)
-        await perpL2Handler.connect(signer).openPosition(true, usdcBal, 500);
-
-        equal((await perpL2Handler.perpPosition()).isActive, true);
-        equal((await perpL2Handler.perpPosition()).isShort, true);
-
-        expect(!((await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV)).isZero()));
-
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0xAE75B29ADe678372D77A8B41225654138a7E6ff1"],
     });
 
-    it("Can close short", async function () {
+    invalidSigner = await hre.ethers.getSigner(
+      "0xAE75B29ADe678372D77A8B41225654138a7E6ff1"
+    );
 
-        let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
+    const PerpHandlerL2 = await hre.ethers.getContractFactory(
+      "PerpPositionHandlerL2",
+      invalidSigner
+    );
+    perpL2Handler = (await PerpHandlerL2.deploy(
+      wantTokenL1,
+      wantTokenL2,
+      invalidSigner.address,
+      perpVault,
+      clearingHouse,
+      clearingHouseConfig,
+      accountBalance,
+      orderBook,
+      exchange,
+      baseToken,
+      quoteTokenvUSDC,
+      signer.address,
+      movrRegistry
+    )) as PerpPositionHandlerL2;
 
-        equal((await perpL2Handler.perpPosition()).isActive, true);
-        await perpL2Handler.connect(signer).closePosition(1500);
-        equal((await perpL2Handler.perpPosition()).isActive, false);
-        expect(((await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV)).isZero()));
+    await perpL2Handler.deployed();
 
-        let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
+    USDC = (await hre.ethers.getContractAt(
+      "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20",
+      wantTokenL2
+    )) as IERC20;
+    await USDC.connect(invalidSigner).transfer(
+      perpL2Handler.address,
+      await USDC.balanceOf(invalidSigner.address)
+    );
 
-        expect(usdcBalAfter.gt(usdcBalBefore))
-    });
+    const baseTokenContract = (await hre.ethers.getContractAt(
+      "IIndexPrice",
+      baseToken
+    )) as IIndexPrice;
+    clearingHouseConfigContract = (await hre.ethers.getContractAt(
+      "IClearingHouseConfig",
+      clearingHouseConfig
+    )) as IClearingHouseConfig;
+    const twapInterval = await clearingHouseConfigContract.getTwapInterval();
+    accountBalanceContract = (await hre.ethers.getContractAt(
+      "IAccountBalance",
+      accountBalance
+    )) as IAccountBalance;
+  });
 
+  it("Contract initialized correctly", async function () {
+    expect(await perpL2Handler.keeper()).equals(signer.address);
 
+    const usdcBal = await USDC.balanceOf(perpL2Handler.address);
 
-    it("Open position only works with keeper", async function () {
+    expect(usdcBal.gt(0));
+  });
 
-        let usdcBal = await USDC.balanceOf(perpL2Handler.address);
-        usdcBal = usdcBal.mul(1e12);
-        await expect(
-            perpL2Handler.connect(invalidSigner).openPosition(true, usdcBal, 500)
-          ).to.be.revertedWith("Only owner can call this function");
-    });
+  it("Can open short", async function () {
+    let usdcBal = BigNumber.from(100e6);
+    // let usdcBal = await USDC.balanceOf(perpL2Handler.address);
+    usdcBal = usdcBal.mul(1e12);
 
-    it("Close position only works with keeper", async function () {
+    equal((await perpL2Handler.perpPosition()).isActive, false);
 
-        let usdcBal = await USDC.balanceOf(perpL2Handler.address);
-        usdcBal = usdcBal.mul(1e12);
-        await expect(
-            perpL2Handler.connect(invalidSigner).closePosition(500)
-          ).to.be.revertedWith("Only owner can call this function");
-    });
+    equal(
+      await accountBalanceContract.getTotalPositionSize(
+        perpL2Handler.address,
+        baseToken
+      ),
+      0
+    );
+    await perpL2Handler.connect(signer).openPosition(true, usdcBal, 500);
 
+    equal((await perpL2Handler.perpPosition()).isActive, true);
+    equal((await perpL2Handler.perpPosition()).isShort, true);
 
+    expect(
+      !(
+        await accountBalanceContract.getTotalPositionSize(
+          perpL2Handler.address,
+          baseToken
+        )
+      ).isZero()
+    );
+  });
 
-    it("Can open long", async function () {
+  it("Can close short", async function () {
+    let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
 
-        let usdcBal = await USDC.balanceOf(perpL2Handler.address);
-        usdcBal = usdcBal.mul(1e12);
+    equal((await perpL2Handler.perpPosition()).isActive, true);
+    await perpL2Handler.connect(signer).closePosition(1500);
+    equal((await perpL2Handler.perpPosition()).isActive, false);
+    expect(
+      (
+        await accountBalanceContract.getTotalPositionSize(
+          perpL2Handler.address,
+          baseToken
+        )
+      ).isZero()
+    );
 
-        equal((await perpL2Handler.perpPosition()).isActive, false);
+    let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
 
-        equal(await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV), 0)
-        await perpL2Handler.connect(signer).openPosition(false, usdcBal, 500);
+    expect(usdcBalAfter.gt(usdcBalBefore));
+  });
 
-        equal((await perpL2Handler.perpPosition()).isActive, true);
-        equal((await perpL2Handler.perpPosition()).isShort, false);
+  it("Open position only works with keeper", async function () {
+    let usdcBal = await USDC.balanceOf(perpL2Handler.address);
+    usdcBal = usdcBal.mul(1e12);
+    await expect(
+      perpL2Handler.connect(invalidSigner).openPosition(true, usdcBal, 500)
+    ).to.be.revertedWith("Only owner can call this function");
+  });
 
-        expect(!((await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV)).isZero()));
+  it("Close position only works with keeper", async function () {
+    let usdcBal = await USDC.balanceOf(perpL2Handler.address);
+    usdcBal = usdcBal.mul(1e12);
+    await expect(
+      perpL2Handler.connect(invalidSigner).closePosition(500)
+    ).to.be.revertedWith("Only owner can call this function");
+  });
 
-    });
+  it("Can open long", async function () {
+    let usdcBal = BigNumber.from(100e6);
+    // let usdcBal = await USDC.balanceOf(perpL2Handler.address);
+    usdcBal = usdcBal.mul(1e12);
 
-    it("Can close long", async function () {
+    equal((await perpL2Handler.perpPosition()).isActive, false);
 
-        let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
+    equal(
+      await accountBalanceContract.getTotalPositionSize(
+        perpL2Handler.address,
+        baseToken
+      ),
+      0
+    );
+    await perpL2Handler.connect(signer).openPosition(false, usdcBal, 500);
 
-        equal((await perpL2Handler.perpPosition()).isActive, true);
-        await perpL2Handler.connect(signer).closePosition(1500);
-        equal((await perpL2Handler.perpPosition()).isActive, false);
-        expect(((await accountBalanceContract.getTotalPositionSize(perpL2Handler.address, baseTokenvCRV)).isZero()));
+    equal((await perpL2Handler.perpPosition()).isActive, true);
+    equal((await perpL2Handler.perpPosition()).isShort, false);
 
-        let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
+    expect(
+      !(
+        await accountBalanceContract.getTotalPositionSize(
+          perpL2Handler.address,
+          baseToken
+        )
+      ).isZero()
+    );
+  });
 
-        expect(usdcBalAfter.gt(usdcBalBefore))
-    });
+  it("Can close long", async function () {
+    let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
 
-    it("Can withdraw successfully", async function () {
+    equal((await perpL2Handler.perpPosition()).isActive, true);
+    await perpL2Handler.connect(signer).closePosition(1500);
+    equal((await perpL2Handler.perpPosition()).isActive, false);
+    expect(
+      (
+        await accountBalanceContract.getTotalPositionSize(
+          perpL2Handler.address,
+          baseToken
+        )
+      ).isZero()
+    );
 
-        let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
+    let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
 
-        const movrData = (await moverCall(perpL2Handler.address, invalidSigner.address, usdcBalBefore , false));
-        const withdrawTxn = await perpL2Handler.connect(signer).withdraw(usdcBalBefore, movrData.target, movrData.registry, movrData.data);
-        let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
+    expect(usdcBalAfter.gt(usdcBalBefore));
+  });
 
-        // console.log(withdrawTxn)
+  it("Can withdraw successfully", async function () {
+    let usdcBalBefore = await USDC.balanceOf(perpL2Handler.address);
 
-        expect(usdcBalAfter.lt(usdcBalBefore))
-    });
+    const movrData = await moverCall(
+      perpL2Handler.address,
+      invalidSigner.address,
+      usdcBalBefore,
+      false
+    );
+    const withdrawTxn = await perpL2Handler
+      .connect(signer)
+      .withdraw(
+        usdcBalBefore,
+        movrData.target,
+        movrData.registry,
+        movrData.data
+      );
+    let usdcBalAfter = await USDC.balanceOf(perpL2Handler.address);
 
+    // console.log(withdrawTxn)
+
+    expect(usdcBalAfter.lt(usdcBalBefore));
+  });
 });
 
 const equal = async function (a, b, reason?) {
-    expect(a).equals(b, reason);
-}
+  expect(a).equals(b, reason);
+};
