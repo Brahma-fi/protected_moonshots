95c536eabf0d313dd3c46652651031129dfd3459
OGZ <adi.gupta13@gmail.com>
2022-03-25 14:03:15 +0530

    Removing hardhat console

--- contracts/PerpHandler/PerpPositionHandler.sol
+++ contracts/PerpHandler/PerpPositionHandler.sol
@@ -8,8 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../PerpL2/interfaces/IPositionHandler.sol";
 
-import "hardhat/console.sol";
-
 /// @title PerpPositionHandlerL1
 /// @author 0xAd1
 /// @notice Used to control the short position handler deployed on Optimism which interacts with PerpV2
@@ -82,15 +80,12 @@ contract PerpPositionHandler is
       data,
       (OpenPositionParams)
     );
-    console.logBytes4(IPositionHandler.openPosition.selector);
     bytes memory L2calldata = abi.encodeWithSelector(
       IPositionHandler.openPosition.selector,
       openPositionParams._isShort,
       openPositionParams._amount,
       openPositionParams._slippage
     );
-
-    console.logBytes(L2calldata);
 
     sendMessageToL2(positionHandlerL2Address, L2calldata, openPositionParams._gasLimit);
   }
