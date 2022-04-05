e9799257affa2fc018b5214e3f12fd1bac7d983a
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-04-05 17:31:33 +0530

    setup for testing

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -3,7 +3,7 @@ export const wantTokenL2 = "0x7F5c764cBc14f9669B88837ca1490cCa17c31607";
 export const ust3Pool = "0xCEAF7747579696A2F0bb206a14210e3c9e6fB269";
 export const curve3PoolZap = "0xA79828DF1850E8a3A3064576f380D90aECDD3359";
 export const baseRewardPool = "0x7e2b9B5244bcFa5108A76D5E7b507CFD5581AD4A";
-
+export const convexBooster = "0xF403C135812408BFbE8713b5A23a04b3D48AAE31";
 export const keeper = "0x55FE002aefF02F77364de339a1292923A15844B8";
 export const governance = "0xAE75B29ADe678372D77A8B41225654138a7E6ff1";
 
