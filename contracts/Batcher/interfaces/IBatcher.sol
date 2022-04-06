ba96afaadb20ae50a0dc7d4580304fa742342c57
OGZ <adi.gupta13@gmail.com>
2022-04-06 20:03:12 +0530

    Formatting acc to doc standard

--- contracts/Batcher/interfaces/IBatcher.sol
+++ contracts/Batcher/interfaces/IBatcher.sol
@@ -7,65 +7,69 @@ pragma solidity ^0.8.0;
  * @dev Provides an interface for Batcher
  */
 interface IBatcher {
-  /**
-   * @notice Stores the deposits for future batching via periphery
-   * @param amountIn Value of token to be deposited
-   * @param signature signature verifying that depositor has enough karma and is authorized to deposit by brahma
-   */
+
+  /// @notice Data structure to store vault info
+  /// @param vaultAddress Address of the vault
+  /// @param tokenAddress Address vault's want token
+  /// @param maxAmount Max amount of tokens to deposit in vault
+  /// @param currentAmount Current amount of wantTokens deposited in the vault
+  struct VaultInfo {
+    address vaultAddress;
+    address tokenAddress;
+    uint256 maxAmount;
+    uint256 currentAmount;
+  }
+
+  /// @notice Deposit event
+  /// @param sender Address of the depositor
+  /// @param hauler Address of the hauler
+  /// @param amountIn Tokens deposited
+  event DepositRequest(
+    address indexed sender,
+    address indexed hauler,
+    uint256 amountIn
+  );
+
+  /// @notice Withdraw event
+  /// @param sender Address of the withdawer
+  /// @param hauler Address of the hauler
+  /// @param amountOut Tokens deposited
+  event WithdrawRequest(
+    address indexed sender,
+    address indexed hauler,
+    uint256 amountOut
+  );
+
+
   function depositFunds(
     uint256 amountIn,
     bytes memory signature
   ) external;
 
-  /**
-   * @notice Stores the deposits for future batching via periphery
-   * @param amountIn Value of Lp token to be deposited
-   * @param signature signature verifying that depositor has enough karma and is authorized to deposit by brahma
-   */
+
   function depositFundsInCurveLpToken(
     uint256 amountIn,
     bytes memory signature
   ) external;
 
-  /**
-   * @notice Allows user to withdraw LP tokens
-   * @param amount Amount of LP tokens to withdraw
-   * @param recipient Address to receive the LP tokens
-   */
+
   function claimTokens(
     uint256 amount,
     address recipient
   ) external;
 
-  /**
-   * @notice Stores the deposits for future batching via periphery
-   * @param amountOut Value of token to be deposited
-   */
+
   function withdrawFunds(uint256 amountOut) external;
 
-  /**
-   * @notice Performs deposits on the periphery for the supplied users in batch
-   * @param users array of users whose deposits must be resolved
-   */
+
   function batchDeposit(address[] memory users) external;
 
-  /**
-   * @notice Performs withdraws on the periphery for the supplied users in batch
-   * @param users array of users whose deposits must be resolved
-   */
+
   function batchWithdraw(address[] memory users)
     external;
 
-  /**
-   * @notice To set a token address as the deposit token for a hauler
-   * @param maxLimit max deposit limit on vault
-   */
+
   function setHaulerLimit(uint256 maxLimit) external;
 
-
-  /**
-   * @notice To set slippage param for curve lp token conversion
-   * @param slippage for curve lp token to usdc conversion
-   */
   function setSlippage(uint256 slippage) external;
 }
