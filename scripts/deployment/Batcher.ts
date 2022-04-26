a106a9614a0e6df118d03bff23ea381b383910a3
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-04-26 16:11:02 +0530

    🚨 batcher conflicts fix

--- scripts/deployment/Batcher.ts
+++ scripts/deployment/Batcher.ts
@@ -1,16 +1,19 @@
 import hre from "hardhat";
+import { BigNumber } from "ethers";
 import { Batcher } from "../../src/types";
 
 async function main() {
   const verificationAuthority = "0x687f4304Df62449dBc6C95FE9A8cb1153d40D42e";
-  const governanceAddress = "0x6b29610D6c6a9E47812bE40F1335918bd63321bf";
+  const vaultAddress = "0x1C4ceb52ab54a35F9d03FcC156a7c57F965e081e";
+  const maxDepositLimit = BigNumber.from(1005000).mul(1e6);
   const Batcher = await hre.ethers.getContractFactory("Batcher");
   const httpsProvider = await hre.ethers.provider;
   let feeData = await httpsProvider.getFeeData();
 
   const batcher = (await Batcher.deploy(
     verificationAuthority,
-    governanceAddress,
+    vaultAddress,
+    maxDepositLimit,
     {
       maxPriorityFeePerGas: feeData["maxPriorityFeePerGas"], // Recommended maxPriorityFeePerGas
       maxFeePerGas: feeData["maxFeePerGas"], // Recommended maxFeePerGas
@@ -24,7 +27,11 @@ async function main() {
 
   await hre.run("verify:verify", {
     address: batcher.address,
-    constructorArguments: [verificationAuthority, governanceAddress],
+    constructorArguments: [
+      verificationAuthority,
+      vaultAddress,
+      maxDepositLimit,
+    ],
   });
 }
 
