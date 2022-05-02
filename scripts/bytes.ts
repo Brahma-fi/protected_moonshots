8a6df9696b98606ba11bb72b7f9d1673ffb3cec1
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-05-02 13:10:17 +0530

    added vault docs

--- scripts/bytes.ts
+++ scripts/bytes.ts
@@ -1,16 +1,18 @@
 import { ethers } from "hardhat";
-
+import { convexTradeExecutorAddress, vaultAddress } from "./constants";
 const getDepositBytes = async () => {
   const convexTE = await ethers.getContractAt(
     "ConvexTradeExecutor",
-    "0x3167b932336b029bBFE1964E435889FA8e595738"
+    convexTradeExecutorAddress
   );
+  const vault = await ethers.getContractAt("Vault", vaultAddress);
   const usdc = await ethers.getContractAt(
     "ERC20",
     "0xceaf7747579696a2f0bb206a14210e3c9e6fb269"
   );
 
-  const usdcBal = await usdc.balanceOf(convexTE.address);
+  let usdcBal = await usdc.balanceOf(convexTE.address);
+  usdcBal = usdcBal.sub(ethers.utils.parseEther("0.1"));
   console.log("Vault USDC bal:", usdcBal.toString());
   const paramsInBytes = ethers.utils.AbiCoder.prototype.encode(
     ["tuple(uint256)"],
