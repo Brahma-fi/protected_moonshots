6232521b37f5c03f1df56daee10b447b2287b733
Bapireddy k <bapir@BapiReddys-Mac.local>
2022-05-02 19:59:04 +0530

    🎨 changes for convex te

--- contracts/ConvexExecutor/interfaces/IHarvester.sol
+++ contracts/ConvexExecutor/interfaces/IHarvester.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.0;
-
+import "../../../interfaces/IVault.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 interface IHarvester {
@@ -10,15 +10,7 @@ interface IHarvester {
 
     function _3crv() external view returns (IERC20);
 
-    function keeper() external view returns (address);
-
-    function governance() external view returns (address);
-
-    function setWantToken(address _addr) external;
-
-    function setKeeper(address _keeper) external;
-
-    function setGovernance(address _governance) external;
+    function vault() external view returns (IVault);
 
     // Swap tokens to wantToken
     function harvest() external;
