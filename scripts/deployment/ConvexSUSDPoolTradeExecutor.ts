73594768767a6ebc8d5cd705e7ab2084e15590ad
pradeep-selva <pradeepselvaraju@gmail.com>
2022-06-23 13:44:01 +0530

    convex te deployed 🔖

--- scripts/deployment/ConvexSUSDPoolTradeExecutor.ts
+++ scripts/deployment/ConvexSUSDPoolTradeExecutor.ts
@@ -1,37 +1,37 @@
 import hre from "hardhat";
 import { Vault } from "../../src/types";
 
-const ConvexFraxPoolTradeExecutorConfig = {
+const ConvexSUSDPoolTradeExecutorConfig = {
   harvester: "0x2022C855CeefD7759dBbb5bB7A8F14C82688646A",
   vault: "0x3c4Fe0db16c9b521480c43856ba3196A9fa50E08",
 };
 
 async function main() {
-  const ConvexFraxPoolTradeExecutor = await hre.ethers.getContractFactory(
-    "ConvexFraxPoolTradeExecutor"
+  const ConvexSUSDPoolTradeExecutor = await hre.ethers.getContractFactory(
+    "ConvexSUSDPoolTradeExecutor"
   );
   const httpsProvider = await hre.ethers.provider;
   let feeData = await httpsProvider.getFeeData();
 
-  const convexFraxPoolTradeExecutor = (await ConvexFraxPoolTradeExecutor.deploy(
-    ...Object.values(ConvexFraxPoolTradeExecutorConfig),
+  const convexSUSDPoolTradeExecutor = (await ConvexSUSDPoolTradeExecutor.deploy(
+    ...Object.values(ConvexSUSDPoolTradeExecutorConfig),
     {
       maxPriorityFeePerGas: feeData["maxPriorityFeePerGas"], // Recommended maxPriorityFeePerGas
       maxFeePerGas: feeData["maxFeePerGas"], // Recommended maxFeePerGas
     }
   )) as Vault;
 
-  await convexFraxPoolTradeExecutor.deployed();
+  await convexSUSDPoolTradeExecutor.deployed();
   console.log(
-    "ConvexFraxPoolTradeExecutor deployed to:",
-    convexFraxPoolTradeExecutor.address
+    "ConvexSUSDPoolTradeExecutor deployed to:",
+    convexSUSDPoolTradeExecutor.address
   );
 
   await new Promise((r) => setTimeout(r, 75000));
 
   await hre.run("verify:verify", {
-    address: convexFraxPoolTradeExecutor.address,
-    constructorArguments: [...Object.values(ConvexFraxPoolTradeExecutorConfig)],
+    address: convexSUSDPoolTradeExecutor.address,
+    constructorArguments: [...Object.values(ConvexSUSDPoolTradeExecutorConfig)],
   });
 }
 
