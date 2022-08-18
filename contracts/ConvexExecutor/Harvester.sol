18223c6446a1747975e55aaab38660dc04de4a9d
pradeep-selva <pradeepselvaraju@gmail.com>
2022-08-18 14:20:31 +0530

    ft: remove logs

--- contracts/ConvexExecutor/Harvester.sol
+++ contracts/ConvexExecutor/Harvester.sol
@@ -10,8 +10,6 @@ import "../../interfaces/IAggregatorV3.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "hardhat/console.sol";
-
 /// @title Harvester
 /// @author PradeepSelva
 /// @notice A contract to harvest rewards from Convex staking position into Want Token
@@ -163,8 +161,6 @@ contract Harvester is IHarvester {
         // swap convex to eth
         if (cvxBalance > 0) {
             uint256 expectedOut = (_getPriceForAmount(cvxEthPrice, cvxBalance));
-            console.log("without slippage: ", expectedOut);
-            console.log("with slippage: ", _getMinReceived(expectedOut));
             cvxeth.exchange(
                 1,
                 0,
